e42b547f110d5b137c7fbae5af0679411e64a16d
Nmich <nmich.cor@gmail.com>
2019-05-21 17:42:35 +0200

    initial commit with bootstrap + app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tp-blog-angular-openclassrooms-v1';
+  title = 'Van Life Blog';
 }
